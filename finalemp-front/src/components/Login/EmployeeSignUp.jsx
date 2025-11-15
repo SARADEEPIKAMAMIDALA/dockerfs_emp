d49084405a570116046897a5511a79ca40ed1d3f
saradeepika03@gmail.com <saradeepika03@gmail.com>
2025-11-16 00:06:17 +0530

    frontup1

--- finalemp-front/src/components/Login/EmployeeSignUp.jsx
+++ finalemp-front/src/components/Login/EmployeeSignUp.jsx
@@ -316,7 +316,7 @@ const EmployeeSignUp = () => {
     setMessage(null);
     setMessageType(null);
     try {
-      const response = await fetch("http://localhost:2025/api/auth/employee/signup", {
+      const response = await fetch("http://localhost:7070/api/auth/employee/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
